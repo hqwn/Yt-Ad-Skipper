d5da9ae7bf87e15d0d875f2c1c162c783134613c
Aryan <aryanjain9818@gmail.com>
2025-07-06 14:13:13 -0600

    Update skipper.py

--- skipper.py
+++ skipper.py
@@ -1,23 +1,23 @@
-#Imports the needed modules
 import pyautogui
 import time
 
-#function to dectect the skip button, if it can see it, if it can then it clicks the button
-def d():
-    for i in ['skip.png', 'skip2.png', 'skip3.png']:
-        center_coords = pyautogui.locateCenterOnScreen(i, confidence=0.85)
-        if center_coords:
-            pyautogui.click(center_coords)
+def main():
+    for i in ['skip.png', 'skip2.png']:
+        o = .8 if i == 'skip.png' else .5
+        cordinates = pyautogui.locateCenterOnScreen(i, confidence= o)
+        if cordinates:
+            pyautogui.click(cordinates)
+            break
         else:
             pass
 
-#main code
+
 while True:
     try:
-        d()
-        #time.sleep() to prevent from double clicking
+        main()
+        # time.sleep to prevent double clicking
         time.sleep(.2)
     except:
-        continue
-    #This is to Prevent Using To Much processing power, because we dont want it to constantly check when 99% of the time you wouldn't get an ad.
-    time.wait(5)
+        continue #was pass, neeeds to be continue
+    
+    time.sleep(5) #You can do whatever but running for lots of time with small wait can affect speed of computer, but only a little
